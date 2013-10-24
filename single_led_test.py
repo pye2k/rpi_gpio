9d9403db6108f7a940f86d021d65231a192da03b
pye2k <pyepye@gmail.com>
2013-10-24 01:44:16 -0700

    Dual LED blinking

--- single_led_test.py
+++ single_led_test.py
@@ -6,8 +6,10 @@ SLEEP_TIME = 1
 
 GPIO.setmode(GPIO.BCM)
 GREEN_LED = 7
+RED_LED = 8
 
 GPIO.setup(GREEN_LED, GPIO.OUT)
+GPIO.setup(RED_LED, GPIO.OUT)
 
 try:
 
@@ -15,8 +17,12 @@ try:
     print "Green light ON"
     GPIO.output(GREEN_LED, True)
     time.sleep(SLEEP_TIME)
-    print "...OFF"
+    print "Red light ON"
+    GPIO.output(RED_LED, True)
+    time.sleep(SLEEP_TIME)
+    print "...ALL OFF"
     GPIO.output(GREEN_LED, False)
+    GPIO.output(RED_LED, False)
     time.sleep(SLEEP_TIME)
 
 except KeyboardInterrupt:
