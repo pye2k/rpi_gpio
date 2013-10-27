3818e3b9194c73f688ba408cb07fa8ff7d91d655
pye2k <pyepye@gmail.com>
2013-10-26 17:28:12 -0700

    Using wiringpi libraries rather than RPi.GPIO

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ rpi_gpio
 ========
 
 GPIO experiments on the Raspberry Pi
+
+Requirements
+------------
+
+* One of RPi.GPIO, or
+> sudo pip install RPi.GPIO
+
+* wiringPi
+> git clone git://git.drogon.net/wiringPi
+> cd wiringPi
+> sudo ./build
+>
+> _Then, the Python wrapper library, WiringPi-Python_
+>
+> sudo pip install wiringpi
+>
+> Or, buid it from source:
+>
+> git clone https://github.com/WiringPi/WiringPi-Python.git
+> cd WiringPi-Python
+> sudo git submodule update --init
+> sudo python setup.py install 
+
+_NOTE:_ wiringPi accesses the GPIO pins differently than RPi.GPIO,
+therefore, you can access the GPIO pins without root access with
+wiringPi.
+
+> e.g.
+> sudo -u <user> /usr/local/bin/gpio export <pin> out
+
--- single_led_test.py
+++ single_led_test.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python
 
-import RPi.GPIO as GPIO, time, os
+import wiringpi, time, os
 
 SLEEP_TIME = 1
 
-GPIO.setmode(GPIO.BCM)
+wiringpi.wiringPiSetupSys()
+
 GREEN_LED = 7
 RED_LED = 8
 
-GPIO.setup(GREEN_LED, GPIO.OUT)
-GPIO.setup(RED_LED, GPIO.OUT)
+wiringpi.pinMode(GREEN_LED, wiringpi.OUTPUT)
+wiringpi.pinMode(RED_LED, wiringpi.OUTPUT)
 
 try:
 
   while True:
     print "Green light ON"
-    GPIO.output(GREEN_LED, True)
+    wiringpi.digitalWrite(GREEN_LED, wiringpi.HIGH)
     time.sleep(SLEEP_TIME)
     print "Red light ON"
-    GPIO.output(RED_LED, True)
+    wiringpi.digitalWrite(RED_LED, wiringpi.HIGH)
     time.sleep(SLEEP_TIME)
     print "...ALL OFF"
-    GPIO.output(GREEN_LED, False)
-    GPIO.output(RED_LED, False)
+    wiringpi.digitalWrite(GREEN_LED, wiringpi.LOW)
+    wiringpi.digitalWrite(RED_LED, wiringpi.LOW)
     time.sleep(SLEEP_TIME)
 
 except KeyboardInterrupt:
-  GPIO.cleanup()
+  wiringpi.digitalWrite(GREEN_LED, wiringpi.LOW)
+  wiringpi.digitalWrite(RED_LED, wiringpi.LOW)
